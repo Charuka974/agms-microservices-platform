cccc81585a717f81e1081ec064b73b381387ad48
Charuka974 <charuka974@gmail.com>
2026-02-27 12:49:12 +0530

    initialize AGMS project structure and domain microservice modules

---
+++ services/crop-service/src/test/java/com/ugch/agms/crop_service/CropServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ugch.agms.crop_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CropServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
