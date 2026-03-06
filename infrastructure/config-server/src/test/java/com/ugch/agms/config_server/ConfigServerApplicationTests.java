0dbc58b9ce8b7795429270b2ef21502f0791cbae
Charuka974 <charuka974@gmail.com>
2026-03-06 21:37:02 +0530

    Initialize config-server with  foundational files and configurations.

---
+++ infrastructure/config-server/src/test/java/com/ugch/agms/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ugch.agms.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
